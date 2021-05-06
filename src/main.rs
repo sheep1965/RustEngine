d7926933c30aa2a929bed8ae791b030afe0fafdc
Jonathan Skopp <zeyecx@gmail.com>
2021-05-06 17:24:16 +0200

    Add std Time to HelloWorld

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 
 fn main() {
-
+    use std::{thread, time};
     println!("Hello World!");
-}
+    let ten_millis = time::Duration::from_millis(1000);
+    let now = time::Instant::now();
+    thread::sleep(ten_millis);
+    assert!(now.elapsed() >= ten_millis);
+ }
